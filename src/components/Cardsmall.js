a0f4d8f955a1d6026e561a5369beb5f3c9ed8157
unknown <isaacagyeman437@gmail.com>
2023-05-21 23:53:53 +0100

    finished positioning background image

--- src/components/Cardsmall.js
+++ src/components/Cardsmall.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ellipsis from '../assets/icon-ellipsis.svg';
+import work from '../assets/icon-work.svg';
 
 export default function Cardsmall(backgroundImage , content){
 return(
     <section className='smallOuter'>
+        <img src={work} className='cardImage'/>
         <div className='smallInner'>
             <header>
                 <p className='cardTitle'>Work</p>
